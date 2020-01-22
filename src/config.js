6067c94bdc2f263e8beb711569e1886a43de472e
Evan Vogts <evanjvogts@gmail.com>
2020-01-22 16:12:25 -0600

    deploy

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin:123@localhost/noteful',
-  API_KEY: process.env.API_Key || 'AIzaSyCG_FMdGssqTRG7tGSvu24UYFopSWQY_-g',
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://dunder_mifflin:123@localhost/noteful'
 };
 // dbname=d4us1m7r3e8tcp host=ec2-34-193-42-173.compute-1.amazonaws.com port=5432 user=vttnrizsydnlfe password=5bfd4884b3d046e35a53f4c82b827d0cf3a246a59b28bcccf5c329e35d4629fb sslmode=require
