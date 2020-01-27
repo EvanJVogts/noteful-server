1c3c618d8afffe89f83d07915b8b9f91bb8d3c25
Evan Vogts <evanjvogts@gmail.com>
2020-01-27 12:19:22 -0600

    deploy

--- src/app.js
+++ src/app.js
@@ -5,47 +5,29 @@ const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
 const foldersRouter = require('./folders/folders-router');
-const noteRouter = require('./notes/notes-router');
-
+const notesRouter = require('./notes/notes-router');
+const knex = require('knex');
+const knexInstance = knex({
+  client: 'pg',
+  connection: process.env.DB_URL,
+});
 const app = express();
-
-const morganOption = (NODE_ENV === 'production')
-  ? 'tiny'
-  : 'common';
-
+const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
 app.use(morgan(morganOption));
-const allowedOrigins = ['https://bookmarks-app.evanjvogts.now.sh/'];
+const allowedOrigins = ['http://localhost:3000', 'http://localhost:3001', 'https://bookmarks-app.evanjvogts.now.sh'];
 app.use(cors({
   origin: function (origin, callback) {
     // allow requests with no origin - like mobile apps, curl, postman
     if (!origin) return callback(null, true);
     if (allowedOrigins.indexOf(origin) === -1) {
       const msg = 'The CORS policy for this site does not ' +
-            'allow access from the specified Origin.';
+          'allow access from the specified Origin.';
       return callback(new Error(msg), false);
     }
     return callback(null, true);
   }
 }));
 app.use(helmet());
-
-app.get('/', (req,res) => {
-  res.send('Hello, world');
-});
-
-app.use(function errorHandler(error, req, res, next) {
-  let response;
-  console.log('error');
-  if (NODE_ENV === 'production') {
-    response = { error: { message: 'server error' } }
-  } else {
-    console.error(error);
-    response = { message: error.message, error }
-  }
-  res.status(500).json(response);
-});
-
 app.use('/api/folders', foldersRouter);
-app.use('/api/notes', noteRouter);
-
+app.use('/api/notes', notesRouter);
 module.exports = app;
